e926b3d61da635855b8df80366a339f3a3b4fabd
Lambda_School_Loaner_252 <c0der.br1t@gmail.com>
2020-01-27 14:44:25 -0600

    Added notes for testing files

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,6 @@
 // Test away
+/*
+- Renders Dashboard component.
+- Shows the Controls and Display.
+- Should default to *unlocked* and *open*
+*/
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,9 @@
 // Test away!
+/*
+- Displays if gate is open/closed
+    - Displays *Closed* is closed prop is true, and *Open* otherwise
+- Displays if gate is locked/unlocked
+    - Displays *Locked* if locked prop is true, and *Unlocked* otherwise
+- When locked or closed, use the *red-led* class
+- when unlocked or open, use the *green-led* class
+*/
